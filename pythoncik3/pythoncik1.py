0e9c0c04e6eb325547c5b484d359e9c97b99df6f
Freezerond <91515706+Freezerond@users.noreply.github.com>
2022-04-13 06:29:06 +0300

    Add files via upload

---
+++ pythoncik3/pythoncik1.py
@@ -0,0 +1,22 @@
+arraychik = open('input1.txt', 'r'). read()
+file = open('output1.txt', 'w+')
+x = 0
+y = 0
+massiv = []
+for i in arraychik :
+    f = [x,y]
+    if (i == '^') :
+        y += 1
+    if (i == '>') :
+        x += 1
+    if (i == 'v') :
+        y -= 1
+    if (i == '<') :
+        x -= 1
+    massiv.append(f)
+massiv0 = []
+for g in massiv :
+    if g not in massiv0 :
+        massiv0.append(g)
+file.write (str(len(massiv0)))
+file.close ()
