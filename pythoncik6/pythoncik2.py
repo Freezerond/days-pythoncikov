8f2fa0c63774cc56493e7e974139befc65e8f48d
Freezerond <91515706+Freezerond@users.noreply.github.com>
2022-04-13 21:28:20 +0300

    Add files via upload

---
+++ pythoncik6/pythoncik2.py
@@ -0,0 +1,23 @@
+import numpy as np
+arraychik = open('input1.txt', 'r')
+file = open('output2.txt', 'w+')
+array = np.zeros((1000, 1000))
+for stroka in arraychik:
+    a = stroka.split()
+    if a[0] == 'turn':
+        x, y = a[2].split(',')
+        z, w = a[4].split(',')
+        x, y, z, w = int(x), int(y), int(z), int(w)
+        if a[1] == 'on':
+            array[x:z+1, y:w+1] += 1
+        elif a[1] == 'off':
+            array[x:z+1, y:w+1] += -1
+            array[array < 0] = 0
+    else:
+        x, y = a[1].split(',')
+        z, w = a[3].split(',')
+        x, y, z, w = int(x), int(y), int(z), int(w)
+        array[x:z+1, y:w+1] += 2
+
+file.write(str(np.sum(array)))
+file.close()
